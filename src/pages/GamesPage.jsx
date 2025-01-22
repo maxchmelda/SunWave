141066b47a3d45a2d507e253768633b9e6b3c511
Max Chmelicek <max.chmelicek@gmail.com>
2025-01-22 19:22:01 +0100

    responsivita

--- src/pages/GamesPage.jsx
+++ src/pages/GamesPage.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { motion } from 'motion/react'
-import { Findus, Footer, Games, Navbar } from '../components'
-import City from '../assets/city.jpg'
-import Logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { motion } from 'motion/react';
+import { Findus, Footer, Games, Navbar } from '../components';
+import City from '../assets/city.jpg';
+import Logo from '../assets/logo.png';
+import { Link } from 'react-router-dom';
 import Fivem_M from '../assets/Fivem_military.png';
 import Fivem_RP from '../assets/Fivem_rp.png';
 import Rust from '../assets/rust.png';
@@ -11,44 +11,47 @@ import Minecraft from '../assets/minecraft.png';
 
 const GamesPage = () => {
   return (
-    <div className='w-full min-h-screen bg-black font-inter'>
-        <img
-          src={City} 
-          alt="background" 
-          className='absolute h-[800px] w-full opacity-30 top-[-120px] z-0'
+<div className='w-full min-h-screen bg-black'>
+        <div 
+          className='absolute h-[400px] w-full sm:top-[100px] top-[50px] z-0 opacity-25'
+          style={{
+            backgroundImage: `url(${City})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
         />
-        <div className=' absolute z-5 top-[450px] w-full h-[350px] bg-gradient-to-b from-[#0000002a] via-[#000000] to-[#00000000]'>
 
-        </div>
-        <div className='flex justify-center items-start gap-20'>
+        <div className='absolute z-5 top-[350px] sm:top-[450px] w-full h-[400px] bg-gradient-to-b from-[#0000002a] via-[#000000] to-[#00000000]' />
+        <div className='absolute z-5 top-[0px] w-full h-[150px] bg-gradient-to-b from-black to-[#0000002a]' />
+
+        <div className='flex justify-around items-start max-[1035px]:items-center max-[1035px]:justify-between max-[1035px]:px-20 max-[530px]:px-2'>
+          <div className='pl-32 max-[530px]:pl-28'>
             <motion.img 
               initial={{ x: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 1, delay: 0.5, ease: "easeIn" }}
               src={Logo} 
               alt="logo" 
-              className='w-[180px] z-10 mt-3' 
+              className='w-[180px] z-10 mt-3 max-[530px]:w-[120px]' 
             />
-            <Navbar />
-        </div>
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.8, delay: 0.5, ease: "easeIn"}}
-        >
-          <div className='text-white absolute top-[460px] left-[25vw] flex justify-center items-center gap-6'>
-            <Link to="/" className="text-white hover:text-gray-400 underline">home</Link>
-            <p>/</p>
-            <p>hry</p>
           </div>
-          <div className='w-full bg-black flex justify-center items-center text-white mt-[300px]'>
-            <div className='w-[40vw] z-10 flex flex-col justify-center gap-10 items-center'>
+          <Navbar />
+        </div>
+        <div>
+            <div className='text-white absolute top-[350px] sm:top-[460px] sm:left-[20vw] left-[5vw] flex justify-center items-center gap-6'>
+                <Link to="/" className="text-white hover:text-gray-400 underline">home</Link>
+                <p>/</p>
+                <p>pravidla</p>
+            </div>
+          <div className='w-full bg-black flex flex-col justify-center items-center text-white mt-[300px]'>
+            <div className='w-[90%] md:w-[60%] z-10 flex flex-col justify-center gap-10 items-center'>
               <h2 className='text-3xl font-bold'>Lorem ipsum</h2>
-              <p className='text-white font-light text-center text-sm'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. </p>
+              <p className='text-white font-light text-center text-sm'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat.</p>
             </div>
           </div>
-        </motion.div>
-        <div className='w-full flex justify-center items-center font-inter gap-4 mt-20 mb-20'>
+        </div>
+        <div className='w-full flex flex-wrap justify-center items-center font-inter gap-4 mt-20 mb-20'>
                 <motion.a
                     href="/hry/fivemrp"
                     initial={{ y: 100, opacity: 0 }}
@@ -117,4 +120,4 @@ const GamesPage = () => {
   )
 }
 
-export default GamesPage
+export default GamesPage;
